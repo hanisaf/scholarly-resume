724610355020401fbaae08d57ac4d8d4ee10e213
Hani Safadi <hanisaf@gmail.com>
2018-09-17 12:05:08 -0400

    progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule }    from '@angular/common/http';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import {KeysPipe, CitesPipe, CoAuthorsPipe, MoneyPipe} from './pipes';
+import {KeysPipe, CitesPipe, CoAuthorsPipe, MoneyPipe, TenurePipe} from './pipes';
 
 import {
   MatTabsModule,
@@ -20,7 +20,8 @@ import {
     KeysPipe,
     CitesPipe,
     MoneyPipe,
-    CoAuthorsPipe
+    CoAuthorsPipe,
+    TenurePipe
   ],
   imports: [
     BrowserModule,
--- src/app/pipes.ts
+++ src/app/pipes.ts
@@ -28,21 +28,47 @@ export class MoneyPipe implements PipeTransform {
   }
 } 
 
+@Pipe({name: 'tenure'})
+export class TenurePipe implements PipeTransform {
+  transform(dates:Array<string|number>) : string {
+    if(dates.length == 2) {
+      let startdate= dates[0]; 
+      let enddate=dates[1];
+      if(startdate && enddate) {
+        if(startdate != enddate)
+          return startdate + "-" + enddate;
+        else return ""+startdate;
+      } else {
+        if(startdate)
+          return ""+startdate;
+        else if(enddate)
+          return ""+enddate;
+        else return "";
+      }
+    }
+    return "";
+  }
+} 
+
 @Pipe({name: 'coauthors'})
 export class CoAuthorsPipe implements PipeTransform {
   static me = "";
   transform(authors: Array<string>): string {
     if(authors) {
       let res = "(joint work with "
+      let marker = false; //ensuring at least one author is added to the list
       for(let person of authors) {
         if(person != CoAuthorsPipe.me) {
           res = res + person + ", ";
+          marker = true;
         }
       }
       res = res.substr(0, res.length-2);
       let i = res.lastIndexOf(",");
       res=res.replace(/,([^,]*)$/," &"+'$1'); 
-      return res + ")";
+      if(marker)
+        return res + ")";
+      else return "";
     }
     else {return ""};
   }
