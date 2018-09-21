81070ac6ef94647d8d88765ece918719e228ecad
hanisaf <hanisaf@gmail.com>
2018-09-21 19:04:20 -0400

    polyfilll IE

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 # if not installed
 #npm install -g angular-cli-ghpages
-ng build --prod --base-href https://hanisaf.github.io/scholarly-resume/
+ng build --prod --base-href /scholarly-resume/
 ngh
