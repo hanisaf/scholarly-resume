cc463bc7ba72a4a05bd6916b1fd006f5882c7849
hanisaf <hanisaf@gmail.com>
2025-01-01 08:34:57 -0500

    updates

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 # if not installed
 #npm install -g angular-cli-ghpages
+export NODE_OPTIONS=--openssl-legacy-provider
 ng build --prod --base-href /scholarly-resume/
 ngh
--- serve.sh
+++ serve.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+export NODE_OPTIONS=--openssl-legacy-provider
 ng serve --port 4200 --host 0.0.0.0 --disable-host-check
